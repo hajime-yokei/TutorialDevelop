9c3e563ea8abd78becf7aa180b34bd03f57ee80b
hajime-yokei <hyokei614@gmail.com>
2023-03-17 21:02:41 +0900

    User登録機能

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -2,6 +2,8 @@ package com.techacademy.service;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
 
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> getUserList(){
         return userRepository.findAll();
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
     }
 
 }
